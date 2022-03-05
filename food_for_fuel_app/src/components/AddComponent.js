4cb0671036eb714e45093a32b68763bedcc2c680
paetinspier <paetin.nelson@gmail.com>
2022-03-05 13:29:31 -0800

    started dynamic table

--- food_for_fuel_app/src/components/AddComponent.js
+++ food_for_fuel_app/src/components/AddComponent.js
@@ -1,4 +1,4 @@
-import Table from 'react-bootstrap/Table';
+import DynamicTable from './DynamicTable'
 import Stack from 'react-bootstrap/Stack';
 import { useState } from 'react'
 
@@ -10,49 +10,7 @@ function AddComponent(){
         <div className='contain'>
             <div className='contain-rad'>
                 <h2 style={{fontWeight: '150'}}>DAILY LOG</h2>
-                <Table striped bordered hover variant="">
-                    <thead>
-                        <tr>
-                            <th>Description</th>
-                            <th>Amount</th>
-                            <th>Unit</th>
-                            <th>Energy KCAL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>apple</td>
-                            <td>1</td>
-                            <td></td>
-                            <td>94.64</td>
-                        </tr>
-                        <tr>
-                            <td>Banana</td>
-                            <td>1</td>
-                            <td></td>
-                            <td>94.64</td>
-                        </tr>
-                        <tr>
-                            <td>Pear</td>
-                            <td>1</td>
-                            <td></td>
-                            <td>94.64</td>
-                        </tr>
-                        <tr>
-                            <td>Grape</td>
-                            <td>1</td>
-                            <td></td>
-                            <td>94.64</td>
-                        </tr>
-                        <tr>
-                            <td>Orange</td>
-                            <td>1</td>
-                            <td></td>
-                            <td>94.64</td>
-                        </tr>
-                    </tbody>
-                </Table>
-
+                <DynamicTable data={dataTable}/>
             </div>
         </div>
     );
